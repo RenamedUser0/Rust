006c7434994bffd7b2c387be2b8d8189f77c2d3d
RenamedUser0 <mafioznik6553@gmail.com>
2026-03-31 18:31:30 +0300

    Update task04.rs

---
+++ src/hackerrank/task04.rs
@@ -0,0 +1,40 @@
+use std::io;
+
+fn gradingStudents(grades: Vec<i32>) -> Vec<i32> {
+    grades
+        .into_iter()
+        .map(|grade| {
+            if grade < 38 {
+                grade
+            } else {
+                let next_multiple = ((grade / 5) + 1) * 5;
+                if next_multiple - grade < 3 {
+                    next_multiple
+                } else {
+                    grade
+                }
+            }
+        })
+        .collect()
+}
+
+fn main() {
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+    let n: usize = input.trim().parse().unwrap();
+
+    let mut grades = Vec::new();
+
+    for _ in 0..n {
+        input.clear();
+        io::stdin().read_line(&mut input).unwrap();
+        let grade: i32 = input.trim().parse().unwrap();
+        grades.push(grade);
+    }
+
+    let result = gradingStudents(grades);
+
+    for grade in result {
+        println!("{}", grade);
+    }
+}
